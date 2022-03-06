cebca948a06f05973cfe1e60568e048f3394507c
Katelyn Bunker <kerbunker@gmail.com>
2022-03-05 21:24:09 -0500

    Started basic nav section

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="flex-row">
+        <li className="mx-2">
+          About me
+        </li>
+        <li className="mx-2">
+          Portfolio
+        </li>
+        <li className="mx-2">
+          Contact
+        </li>
+        <li className="mx-2">
+          Resume
+        </li>
+      </ul>
+    </nav>
+  )
+}
