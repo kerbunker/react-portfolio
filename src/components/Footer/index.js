b03cc8a0425cf9bf5c775df20dfedaf3d5edca60
Katelyn Bunker <kerbunker@gmail.com>
2022-03-08 19:34:48 -0500

    Finish styling portfolio page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import gitIcon from '../../assets/icons/GitHub-Mark-32px.png';
-import linkedIcon from '../../assets/icons/LI-In-Bug.png';
+import linkedIcon from '../../assets/icons/linkedin.png';
 
 function Footer() {
   return (
     <footer>
       <div className='footer-icon'>
-        <a href="http://github.com/kerbunker">
+        <a href="http://github.com/kerbunker" target="_blank">
           <img
             src={gitIcon}
             alt="github icon"
@@ -15,7 +15,7 @@ function Footer() {
         </a>
       </div>
       <div className='footer-icon'>
-        <a href="https://www.linkedin.com/in/katelyn-bunker-3bb51599/">
+        <a href="https://www.linkedin.com/in/katelyn-bunker-3bb51599/" target="_blank">
         <img
             src={linkedIcon}
             alt="Linkedin icon"
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import gitIcon from '../../assets/icons/GitHub-Mark-Light-32px.png';
+import React, { useState } from "react";
+import gitIcon from "../../assets/icons/GitHub-Mark-Light-32px.png";
 //import Project from '../ProjectList';
 //import { capitalizeFirstLetter } from '../../utils/helpers';
 
@@ -10,68 +10,70 @@ function Portfolio() {
       description: "Node/SQL",
       image_desc: "Landing page for Game Corner",
       repo_url: "https://github.com/kerbunker/game-collection",
-      url: "https://afternoon-reef-79835.herokuapp.com/"
+      url: "https://afternoon-reef-79835.herokuapp.com/",
     },
     {
       name: "Hangman's Revenge",
       description: "JavaScript/API",
       image_desc: "A new game with no quesses made",
       repo_url: "https://github.com/CyanideTheJuggla/hangmans-revenge",
-      url: "https://cyanidethejuggla.github.io/hangmans-revenge/"
+      url: "https://cyanidethejuggla.github.io/hangmans-revenge/",
     },
     {
       name: "Weather Dashbaord",
       description: "API",
       image_desc: "The weather for 5 days in Raleigh",
       repo_url: "https://github.com/kerbunker/weather-dashboard",
-      url: "https://kerbunker.github.io/weather-dashboard/"
+      url: "https://kerbunker.github.io/weather-dashboard/",
     },
     {
       name: "Password Generator",
       description: "Javascript",
       image_desc: "Password generated with 3 of 4 options",
       repo_url: "https://github.com/kerbunker/Password-Generator",
-      url: "http://kerbunker.github.io/Password-Generator/"
+      url: "http://kerbunker.github.io/Password-Generator/",
     },
     {
       name: "Budgeter's Friend",
       description: "PWA",
       image_desc: "Budget with five data points",
       repo_url: "https://github.com/kerbunker/budgeters-friend",
-      url: "https://nameless-thicket-77989.herokuapp.com/"
-    }
+      url: "https://nameless-thicket-77989.herokuapp.com/",
+    },
   ]);
 
   const [currentProject, setCurrentProject] = useState(projects[0]);
 
   return (
-    <div className="portfolio-container">
-      <h1 className='portfolio-header'>Recent Work</h1>
-      {projects.map((proj, i) => (
-        <div className='img-wrap'>
-        <img
-          src={require(`../../assets/images/${i+1}.PNG`)}
-          alt={proj.image_desc}
-          className="mx-1"
-          key={proj.name}
-          height={300}
-          width={400}
-        />
-        <div className='img-description'>
-          <div>
-          <a href={proj.url} className="img-link">{proj.name}</a>
-          <a href={proj.repo_url} className="repo-link">
-            <img src={gitIcon} alt="github icon" />
-          </a>
+    <section className="portfolio-section">
+      <div className="portfolio-container">
+        <h1 className="portfolio-header">Recent Work</h1>
+        {projects.map((proj, i) => (
+          <div className="img-wrap">
+            <img
+              src={require(`../../assets/images/${i + 1}.PNG`)}
+              alt={proj.image_desc}
+              className="img"
+              key={proj.name}
+              height={300}
+              width={400}
+            />
+            <div className="img-description">
+              <div>
+                <a href={proj.url} target="_blank" className="img-link">
+                  {proj.name}
+                </a>
+                <a href={proj.repo_url} target="_blank" className="img-link">
+                  <img src={gitIcon} alt="github icon" />
+                </a>
+              </div>
+              <p>{proj.description}</p>
+            </div>
           </div>
-          <p>{proj.description}</p>
-        </div>
-        </div>
-      ))}
-      <p>Portfolio page</p>
-
-    </div>
+        ))}
+      </div>
+    </section>
   );
-};
+}
 
 export default Portfolio;
